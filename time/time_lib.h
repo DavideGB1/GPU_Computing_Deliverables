9a662327b0607b1d17eba944e37912a2a4593cce
davide <davicolosimo23@gmail.com>
2026-05-03 17:43:34 +0200

    aaaaaaaaaaaaaaaaaaaaaaa

--- time/time_lib.h
+++ time/time_lib.h
@@ -1,17 +1,23 @@
 #ifndef LAB3_EX1_LIB
 #define LAB3_EX1_LIB
+
 #include <stdio.h>
 #include <sys/time.h>
+#include <unistd.h> // Per sleep()
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 #define STR(s) #s
 #define XSTR(s) STR(s)
 
-#define TIMER_DEF(n)	 struct timeval temp_1_##n={0,0}, temp_2_##n={0,0}
-#define TIMER_START(n)	 gettimeofday(&temp_1_##n, (struct timezone*)0)
-#define TIMER_STOP(n)	 gettimeofday(&temp_2_##n, (struct timezone*)0)
+#define TIMER_DEF(n)     struct timeval temp_1_##n={0,0}, temp_2_##n={0,0}
+#define TIMER_START(n)   gettimeofday(&temp_1_##n, (struct timezone*)0)
+#define TIMER_STOP(n)    gettimeofday(&temp_2_##n, (struct timezone*)0)
 #define TIMER_ELAPSED(n) ((temp_2_##n.tv_sec-temp_1_##n.tv_sec)*1.e6+(temp_2_##n.tv_usec-temp_1_##n.tv_usec))
+
+// Nota: Assicurati di includere mpi.h nel file .c/.cu o qui se usi questa macro
 #define TIMER_PRINT(n) \
     do { \
         int rk;\
@@ -21,15 +27,19 @@ extern "C" {
         sleep(0.5);\
         MPI_Barrier(MPI_COMM_WORLD);\
     } while (0);
+
 typedef enum { FORMAT_COO, FORMAT_CSR, FORMAT_ELL } SpMVFormat;
 
-// Put here the declaration of arithmetic_mean and geometric_mean
-double geometric_mean(double array[],int len);
-double arithmetic_mean(double array[],int len);
-double standard_deviation(double array[],double mean, int len);
-double relative_error(double cpu_result[],double gpu_result[], int len);
-void print_stats(const char *label, double *timers, double *errors,int nnz, int n_row,
-                 const char *matrix_name, FILE *csv, SpMVFormat format, int NITER);
-// ----------------------------------------------
+double geometric_mean(double array[], int len);
+double arithmetic_mean(double array[], int len);
+double standard_deviation(double array[], double mean, int len);
+double relative_error(double cpu_result[], double gpu_result[], int len);
 
+void print_stats(const char *label, double *timers, double *errors, int nnz, int n_row,
+                 const char *matrix_name, FILE *csv, SpMVFormat format, int NITER);
+
+#ifdef __cplusplus
+} // Chiusura corretta di extern "C"
 #endif
+
+#endif // Chiusura corretta di LAB3_EX1_LIB
