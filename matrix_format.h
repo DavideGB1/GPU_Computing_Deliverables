879afd81378fbb9d974e6c48bce56cd87a9e7256
davide <davicolosimo23@gmail.com>
2026-05-03 14:46:01 +0200

    matrix format

--- matrix_format.h
+++ matrix_format.h
@@ -1,8 +1,10 @@
-
-
 #ifndef MATRIX_FORMAT_H
 #define MATRIX_FORMAT_H
 #include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct {
     int *rows;
@@ -22,20 +24,17 @@ typedef struct {
     int n_col;
 } CSR_Matrix;
 
-
 COO_Matrix* create_COO(int nnz, int n_row, int n_col);
-
 void free_COO(COO_Matrix *coo);
-
 void sort_COO(COO_Matrix *coo);
-
 void print_COO(COO_Matrix *coo);
-
 CSR_Matrix* create_CSR(COO_Matrix* coo);
-
 void free_CSR(CSR_Matrix *csr);
-
 void print_CSR(CSR_Matrix *csr);
-
 COO_Matrix* mm_parser(FILE *file);
-#endif //MATRIX_FORMAT_H
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
