21ba12ea06973f4b6cf442abfe5da0c4a616f8eb
davide <davicolosimo23@gmail.com>
2026-05-06 12:26:51 +0200

    A100 not available

--- sbatch.sh
+++ sbatch.sh
@@ -3,10 +3,10 @@
 #SBATCH --account=gpu.computing26
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:a100.80:1
+#SBATCH --gres=gpu:a30.24:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=00:05:00
-#SBATCH --job-name=test
+#SBATCH --job-name=SpMV-thermomech_dM
 #SBATCH --output=logs/test-%j.out
 #SBATCH --error=logs/test-%j.err
 
